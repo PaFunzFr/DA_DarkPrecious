0f57cce4255c8347dc384ba548636fea3439ebf0
funZ <153361103+PaFunzFr@users.noreply.github.com>
2025-01-22 09:04:47 +0100

    added category to database

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
-let dbLocalStorage = localStorage.setItem("products", JSON.stringify(database));
+//get from db.js and set to localstorage
+let dbToLocalStorage = localStorage.setItem("database", JSON.stringify(database));
+
+// get from localstorage and parse as array
+let dbFromLocalStorage = JSON.parse(localStorage.getItem("database"));
+
+// define categories of localstorage
+let dbCoffe = dbFromLocalStorage[0];
+let dbCoffeeMachines = dbFromLocalStorage[1];
+
+console.log(dbCoffe.products);
+console.log(dbFromLocalStorage.indexOf("kaffeemaschinen", ));
+
+
+function renderInit() {
+
+}
+
+function renderProductCards(index, category) {
+    getContainerById("favouriteProducts").innerHTML = ""
+    log.info(dbLocalStorage.getItem("products"));
+
+    
+}
+
+function getContainerById(id) {
+    return document.getElementById(id);
+}
